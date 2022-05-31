b9d262c8b33eb34ddb1554e288bef953eae4ade6
CesarWatrin <cesar.watrin@hotmail.com>
2022-05-31 15:33:43 +0200

    feat: 🎸 display results without pressing enter

--- src/app/templates/header/header.component.ts
+++ src/app/templates/header/header.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
-import {firstValueFrom} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from 'src/app/services/auth.service';
 
 @Component({
@@ -27,9 +26,7 @@ export class HeaderComponent implements OnInit {
     onEnter() {
         this.router
             .navigate(['/'], {queryParams: {search: this.search}})
-            .then(() => {
-                window.location.reload();
-            });
+            .then(() => {});
     }
 
     logout() {
