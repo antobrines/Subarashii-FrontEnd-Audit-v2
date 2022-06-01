d5c99e1e0fa329708a971018797ac22f78961e1f
jemerardprete <julie.emerard@sfr.fr>
2022-06-01 17:09:49 +0200

    feat: 🎸 add functions to manage user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,9 @@
-import {ResponseService} from './response.service';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {JwtHelperService} from '@auth0/angular-jwt';
-import {firstValueFrom} from 'rxjs';
-import {environment} from 'src/environments/environment';
+import { ResponseService } from './response.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { firstValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import jwt_decode from 'jwt-decode';
 
 @Injectable({
@@ -14,29 +14,32 @@ export class AuthService {
         private http: HttpClient,
         private jwtHelper: JwtHelperService,
         private responseS: ResponseService
-    ) {
-    }
+    ) {}
 
     async login(data: any): Promise<boolean> {
         try {
             const request = this.http.post(
-                environment.backUrl + 'users/sign-in',
+                environment.backUrl + 'users/login',
                 data
             );
             const dataRequest: any = await firstValueFrom(request);
-            localStorage.setItem('token', dataRequest.body.token);
+            localStorage.setItem('token', dataRequest.body);
             this.responseS.SuccessF(dataRequest);
             return true;
         } catch (error: any) {
+            if (error.error.message == 'Vous êtes banni') {
+                window.location.href = '/banned';
+            }
             this.responseS.ErrorF(error.error);
             return false;
         }
     }
 
     async register(data: any): Promise<boolean> {
-        let headers = new HttpHeaders();
-        headers = headers.set('Content-Type', 'application/json');
-        const request = this.http.post(environment.backUrl + 'users/sign-up', data);
+        const request = this.http.post(
+            environment.backUrl + 'users/register',
+            data
+        );
         try {
             const res = await firstValueFrom(request);
             this.responseS.SuccessF(res);
@@ -60,8 +63,70 @@ export class AuthService {
 
     public userConnected(): any {
         const token: any = localStorage.getItem('token');
-        const decodedToken: any = jwt_decode(token, {header: true});
+        const decodedToken: any = jwt_decode(token, { header: true });
         return decodedToken;
     }
 
+    public async getUser() {
+        try {
+            const $get = this.http.get(environment.backUrl + 'users/me');
+            const data: any = await firstValueFrom($get);
+            return data.body;
+        } catch (error) {
+            return this.responseS.ErrorF(error);
+        }
+    }
+
+    public async updateUser(dataForm: any) {
+        try {
+            const $put = this.http.put(environment.backUrl + 'users', dataForm);
+            const res = await firstValueFrom($put);
+            this.responseS.SuccessF(res);
+            return true;
+        } catch (error) {
+            return this.responseS.ErrorF(error);
+        }
+    }
+
+    public async updateUserPassword(dataForm: any) {
+        try {
+            const $put = this.http.put(
+                environment.backUrl + 'users/password',
+                dataForm
+            );
+            const res = await firstValueFrom($put);
+            this.responseS.SuccessF(res);
+            return true;
+        } catch (error) {
+            return this.responseS.ErrorF(error);
+        }
+    }
+
+    public async forgotPassword(email: any) {
+        try {
+            const $post = this.http.post(
+                environment.backUrl + 'users/password/reset/key',
+                email
+            );
+            const res = await firstValueFrom($post);
+            this.responseS.SuccessF(res);
+            return true;
+        } catch (error) {
+            return this.responseS.ErrorF(error);
+        }
+    }
+
+    public async resetPassword(email: any) {
+        try {
+            const $post = this.http.post(
+                environment.backUrl + 'users/password/reset',
+                email
+            );
+            const res = await firstValueFrom($post);
+            this.responseS.SuccessF(res);
+            return true;
+        } catch (error) {
+            return this.responseS.ErrorF(error);
+        }
+    }
 }
